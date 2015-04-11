90da6c8939f90ff9284f6c24f41d5517d87b256b
Bruno Nuzzi <brunonuzzi@gmail.com>
2015-04-11 23:57:24 +0200

    Update Program.cs

--- Scrapping/Program.cs
+++ Scrapping/Program.cs
@@ -14,7 +14,7 @@ namespace Scrapping
         {
 
            SegundaMano SegundaMano = new SegundaMano();
-           SegundaMano.ScrapearListaPropriedas();
+           //SegundaMano.ScrapearListaPropriedas();
            
 
            //SegundaMano.ScrapearPropriedadesMasivo(1);
